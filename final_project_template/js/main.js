0328e7b811face1016225257ba31096021fc00ad
Eugenia Sanon <eugeniasanon@gmail.com>
2018-03-26 19:35:30 -0400

    final_project_cubablog
    My final Project is a simple blog design to inform.

--- final_project_template/js/main.js
+++ final_project_template/js/main.js
@@ -1,1 +1,66 @@
-// Make it rain!
+// Event for when the form is submitted
+$('form').on('submit', function(event) {
+  event.preventDefault();
+
+  var email = $('#email').val();
+  var name = $('#name').val();
+  var message = $('#message').val();
+
+
+  if (!name) {
+    alert('Please enter name');
+    $('#name').addClass('warn');
+  } else {
+    $('#name').removeClass('warn');
+    alert('Submitted!');
+  }
+
+  if (!email) {
+    alert('Please enter email');
+    $('#email').addClass('warn');
+  } else {
+    $('#email').removeClass('warn');
+    alert('Submitted!');
+  }
+  if (!message) {
+    alert('Please enter message');
+    $('#message').addClass('warn');
+  } else {
+    $('#message').removeClass('warn');
+    alert('Submitted!');
+  }
+});
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
